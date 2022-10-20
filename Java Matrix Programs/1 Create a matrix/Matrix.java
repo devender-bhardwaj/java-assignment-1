8fc715cb5808b57cbcae9c7959f7b9afea8171a0
Devender <25636devender.2020@gmail.com>
2022-10-20 21:36:44 +0530

    First commit

---
+++ Java Matrix Programs/1 Create a matrix/Matrix.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Matrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{3,4,5}};
+        System.out.println(Arrays.toString(matrix));
+    }
+}
